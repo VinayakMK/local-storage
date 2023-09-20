9cc0106896f5f2d20f3f94bbbfe2dd6c640cbec7
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-21 00:00:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+localStorage.setItem("user","Entri")
+localStorage.setItem(27,"Entri")
+
+let res = localStorage.getItem("age");
+
+localStorage.length;
+
+/*localStorage.setItem('user', 'beef');
+localStorage.setItem('age', 23);
+localStorage.setItem('high-score', 0);
+
+// get item
+let age = localStorage.getItem('age');
+console.log(age);
+
+//remove item
+localStorage.removeItem('high-score');
+// localStorage.clear; removes everything
+
+//no of items
+let len = localStorage.length;
+console.log(len);
+
+//clear everything
+localStorage.clear();*/
